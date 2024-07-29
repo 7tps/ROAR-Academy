4099671d43a07e22b062cc053bddceef964476a6
7tps <109924342+7tps@users.noreply.github.com>
2024-07-28 17:36:32 -0700

    Update ex11-3.py

--- exercises/ex11-3.py
+++ exercises/ex11-3.py
@@ -8,6 +8,8 @@ file_handle = open(filename, 'rb')
 pdfReader = PyPDF2.PdfReader(file_handle) 
 page_number = len(pdfReader.pages)   # this tells you total pages 
 
+frequency_table = dict()
+
 for x in range(page_number):
 
     page_object = pdfReader.pages[x]    # We just get page 0 as example 
@@ -16,6 +18,15 @@ for x in range(page_number):
     page_text = page_text.split('\n')
     page_text = page_text[2:]
 
-    page_text = [line for line in page_text if 'CHAPTER' not in line]
+    word_list = [line.split() for line in page_text if 'CHAPTER' not in line and not any(char.isdigit() for char in line)]
 
-    print(page_text)
+    print(word_list)
+
+    for sublist in word_list:
+        for word in sublist:
+            if word in frequency_table:
+                frequency_table[word] += 1
+            else:
+                frequency_table[word] = 1
+
+    print(frequency_table)
